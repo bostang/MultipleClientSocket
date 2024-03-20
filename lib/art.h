dec327b5b1c6d3cf16308d180df7f62c083c014c
Bostang Palaguna <bostangsteiitb2020@gmail.com>
2024-03-20 17:50:34 +0700

    +referensi

--- lib/art.h
+++ lib/art.h
@@ -1,1 +1,12 @@
+// DEKLARASI KONSTANTA
+
+// Daftar nama Developer
+const char nama_developer1[]="Bostang Palaguna";
+const char nama_developer2[]="Karma Kunga";
+const char nama_developer3[]="Emmanuella Pramudita Rumanti";
+const char NIM_developer1[]="13220055";
+const char NIM_developer2[]="13220028";
+const char NIM_developer3[]="13220031";
+
+// INISIASI FUNGSI
 void cetakBanner();
